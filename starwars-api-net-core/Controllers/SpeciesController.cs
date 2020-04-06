90959054b99ed81ad39b6e2c8adbacef6520de04
ruben1985 <rubendmatos1985@gmail.com>
2020-04-06 15:27:53 +0200

    add new specie controller

--- starwars-api-net-core/Controllers/SpeciesController.cs
+++ starwars-api-net-core/Controllers/SpeciesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using starwars_api_net_core.Models;
+using starwars_api_net_core.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +14,12 @@ namespace starwars_api_net_core.Controllers
   public class SpeciesController : ControllerBase
   {
     ISpeciesRepository _speciesRepository;
+    StarwarsContext _context;
 
-    public SpeciesController(ISpeciesRepository speciesRepository)
+    public SpeciesController(ISpeciesRepository speciesRepository, StarwarsContext ctx)
     {
       _speciesRepository = speciesRepository;
+      _context = ctx;
     }
 
     [Route("")]
@@ -34,6 +37,45 @@ namespace starwars_api_net_core.Controllers
         return Ok(new { status = "success", data = result });
       }
       return Ok(new { status = "error", data = "Not Found" });
+    }
+
+    [HttpPost]
+    [Route("add")]
+    public async Task<IActionResult> Add([FromBody] SpecieResponseViewModel specie)
+    {
+      var (Name, Classification, Designation, AverageHeight, SkinColors, EyeColors, HairColors, AverageLifespan, Homeworld, Language, People, Films) = specie;
+      var newSpecie = new Specie
+      {
+        Name = Name,
+        Classification = Classification,
+        Designation = Designation,
+        AverageHeight = AverageHeight,
+        SkinColors = SkinColors,
+        EyeColors = EyeColors,
+        HairColors = HairColors,
+        AverageLifespan = AverageLifespan,
+        Language = Language
+      };
+
+      if (Homeworld != null)
+      {
+        var specieHomeworld = new Planet { Id = Homeworld.Id };
+        _context.Planets.Attach(specieHomeworld);
+        newSpecie.Homeworld = specieHomeworld;
+      };
+
+      if (Films != null)
+      {
+        var specieFilms = Films.Select(f => new FilmSpecie { FilmId = f.Id, SpecieId = newSpecie.Id }).ToList();
+        newSpecie.Films = specieFilms;
+      }
+
+      var specieAdded = await _speciesRepository.Add(newSpecie);
+      if (specieAdded)
+      {
+        return RedirectToAction(nameof(Get), new { Id = newSpecie.Id });
+      }
+      return Ok(new { status = "error", data = "error adding specie" });
     }
   }
 }
