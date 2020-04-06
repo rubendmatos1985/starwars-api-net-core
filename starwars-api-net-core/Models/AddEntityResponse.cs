de224b4052f5dc94719ec9386e6cbab00ddb51d3
ruben1985 <rubendmatos1985@gmail.com>
2020-04-06 18:05:41 +0200

    Deconstruct Method added

--- starwars-api-net-core/Models/AddEntityResponse.cs
+++ starwars-api-net-core/Models/AddEntityResponse.cs
@@ -7,7 +7,13 @@ namespace starwars_api_net_core.Models
 {
   public class AddEntityResponse<T>
   {
-    public bool EntitySuccessfullyAdded { get; set;  }
+    public bool EntitySuccessfullyAdded { get; set; }
     public T Entity { get; set; }
+
+    public void Deconstruct(out bool entSuccc, out T entity)
+    {
+      entity = Entity;
+      entSuccc = EntitySuccessfullyAdded;
+    }
   }
 }
