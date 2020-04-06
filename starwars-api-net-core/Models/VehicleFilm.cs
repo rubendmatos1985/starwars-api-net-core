99af8b835e5340390c4d00ab37992b0b231eb837
ruben1985 <rubendmatos1985@gmail.com>
2020-04-06 16:48:45 +0200

    Many to Many VehicleFilms and VehiclePilots created

---
+++ starwars-api-net-core/Models/VehicleFilm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace starwars_api_net_core.Models
+{
+  public class VehicleFilm
+  {
+    public Guid FilmId { get; set; }
+    public Film Film { get; set;  }
+
+    public Guid VehicleId { get; set;  }
+    public Vehicle Vehicle { get; set; }
+  }
+}
