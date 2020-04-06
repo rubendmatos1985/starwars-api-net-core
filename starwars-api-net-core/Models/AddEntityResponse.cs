5d8605d65363c942bac3bc291d03fd75d3c0435c
ruben1985 <rubendmatos1985@gmail.com>
2020-04-06 16:02:04 +0200

    AddEntityResponse created

---
+++ starwars-api-net-core/Models/AddEntityResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace starwars_api_net_core.Models
+{
+  public class AddEntityResponse<T>
+  {
+    public bool EntitySuccessfullyAdded { get; set;  }
+    public T Entity { get; set; }
+  }
+}
